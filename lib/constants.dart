25e1a40792c08737342fbea5dde58cefe648e205
OutdatedGuy <everythingoutdated@gmail.com>
2023-05-07 18:31:38 +0530

    refactor: remove unused constant variables

--- lib/constants.dart
+++ lib/constants.dart
@@ -5,5 +5,3 @@ const String _webFolder = 'web/';
 const String _webSplashFolder = '${_webFolder}splash/';
 const String _webSplashImagesFolder = '${_webSplashFolder}img/';
 const String _webIndex = '${_webFolder}index.html';
-const String _webRelativeStyleFile = 'splash/style.css';
-const String _webRelativeJSFile = 'splash/splash.js';
